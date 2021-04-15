2370f841fdc9a2f8864e9bcff6603748bb7be6fa
shinkin <53311354+shinkin@users.noreply.github.com>
2021-04-15 11:37:24 +0400

    Delete task4.py

--- task4.py
+++
@@ -1,9 +0,0 @@
-n = int(input("введите целое положительное число: "))
-while n > 0 and isinstance(n, int) :
-    s = str(n)
-    ls = list(map(int, s))
-    r = max(ls)
-    print(r, "самое большая цифра")
-    break
-print(n, "число введеное Вами")
-
