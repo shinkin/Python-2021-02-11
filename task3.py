50d5ae7e52210b1f8e7378ed7a92e5a7b4c4863a
shinkin <53311354+shinkin@users.noreply.github.com>
2021-04-19 10:16:51 +0400

    Add files via upload

--- task3.py
+++ task3.py
@@ -1,35 +1,16 @@
-"""
-Пользователь вводит месяц в виде целого числа от 1 до 12.
-Сообщить к какому времени года относится месяц (зима, весна,
-лето, осень). Напишите решения через list и через dict.
-"""
-
-seasons_list = [
-    ['Зима', ['12', '1', '2']],
-    ['Весна', ['3', '4', '5']],
-    ['Лето', ['6', '7', '8']],
-    ['Осень', ['9', '10', '11']]
-]
-
-seasons_dict = {
-    'Зима': ['12', '1', '2'],
-    'Весна': ['3', '4', '5'],
-    'Лето': ['6', '7', '8'],
-    'Осень': ['9', '10', '11']
-}
-
-while True:
-    month_number = input('Пожалуйста введите номер месяца: ')
-    if month_number not in sum(seasons_dict.values(), []):
-        print('Неправильно введенный номер месяца. Попробуйте еще раз')
-        continue
-
-    break
-
-for season, months in seasons_list:
-    if month_number in months:
-        print(f'Через список определено, что месяц с номером {month_number} это {season}')
-
-for season, months in seasons_dict.items():
-    if month_number in months:
-        print(f'Через словарь определено, что месяц с номером {month_number} это {season}')
+'''
+Реализовать функцию my_func(), которая принимает три позиционных
+аргумента, и возвращает сумму наибольших двух аргументов.
+'''
+print("ведите три аргумента в виде любых целых чисел")
+def my_func(arg1 , arg2, arg3):
+    if arg1 >= arg3 and arg2 >= arg3:
+        return arg1 + arg2
+    elif arg1 > arg2 and arg1 < arg3:
+        return arg1 + arg3
+    else:
+        return arg2 + arg3
+try:
+    print(f'Сумма наибольших двух аргументов равна- {my_func(int(input("введите первый аргумент ")), int(input("введите второй аргумент ")), int(input("введите третий аргумент ")))}')
+except ValueError:
+    print("Ошибка! Вводимые данные должны быть целым числом")
